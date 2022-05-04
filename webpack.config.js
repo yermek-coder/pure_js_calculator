36ba218fab66d0e2dfb369832da88c299ddf9aa0
yermek-dev <mynameisermek@gmail.com>
2022-05-04 20:37:21 +0600

    completelly remade setup, added sass compiler

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,52 @@
-const path = require('path')
-
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+require("babel-polyfill");
 module.exports = {
-	"mode": none,
-	"entry": ".src/index.js",
-	"output": {
-		"path": __dirname + 'dist',
-		"filename": "bundle.js"
-	},
-	"devserver": {
-		contentBase: path.join(__dirname, 'dist')
-	}
-}
+  entry: ["babel-polyfill", __dirname + "/src/app/index.js"],
+  output: {
+    path: __dirname + "/dist",
+    filename: "bundle.js",
+    publicPath: "/",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: "babel-loader",
+        exclude: [/node_modules/],
+      },
+      {
+        test: /\.(sa|sc|c)ss$/,
+        use: [
+          {
+            loader: "style-loader",
+          },
+          {
+            loader: "css-loader",
+          },
+          {
+            loader: "sass-loader",
+          },
+        ],
+      },
+      {
+        test: /\.css$/,
+        use: ExtractTextPlugin.extract({
+          fallback: "style-loader",
+          use: [{ loader: "css-loader" }, { loader: "sass-loader" }],
+        }),
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: __dirname + "/src/public/index.html",
+      inject: "body",
+    }),
+    new ExtractTextPlugin("main.css"),
+  ],
+  devServer: {
+    contentBase: "./src/public",
+    port: 8000,
+  },
+};
