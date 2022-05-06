31d554eaf5198b3d588eeb5e6d2f3c855a0a0781
yermek-dev <mynameisermek@gmail.com>
2022-05-06 17:27:47 +0600

    started with simple sass compiler, production build working just fine now

--- webpack.config.js
+++ webpack.config.js
@@ -1,1 +1,20 @@
+const webpack = require("webpack");
+const path = require("path");
 
+const config = {
+  entry: "./src/app/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+    ],
+  },
+};
+
+module.exports = config;
